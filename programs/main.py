00c7ad5146481fc0babdadf2cb68ada63c1c9bff
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-05 21:13:44 +0900

    5th commit

--- programs/main.py
+++ programs/main.py
@@ -9,3 +9,18 @@ img_height = 180
 img_width = 180
 
 classes=['red panda','raccoon']
+
+num_classes = 5
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
