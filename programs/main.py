7870afa38e5419faed2fc8cfdfd54ca747af8b0f
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-05 21:23:25 +0900

    9th commit

--- programs/main.py
+++ programs/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import PIL
 import tensorflow as tf
 import pathlib
-data_dir = pathlib.Path('C:/vscode/python/Keras2/Image classification with Keras/image')
+data_dir = pathlib.Path('C:/vscode/python/Keras2/Image classification with Keras/images')
 batch_size = 32
 img_height = 180
 img_width = 180
