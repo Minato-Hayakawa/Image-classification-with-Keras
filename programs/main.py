bea8162469c750de50dc504f03950f6f2f310eee
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-05 21:19:02 +0900

    8th commit

--- programs/main.py
+++ programs/main.py
@@ -8,6 +8,21 @@ batch_size = 32
 img_height = 180
 img_width = 180
 
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 classes=['red panda','raccoon']
 
 num_classes = 5
