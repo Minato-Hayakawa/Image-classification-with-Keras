892bc84c3629d8621a5d7564ba828dc70ac84dcc
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-07 17:30:35 +0900

    app.pyを追加

---
+++ programs/app.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+import pathlib
+
+data_dir=pathlib.Path('Input Picture Path')
+new_model = tf.models.load_model('path_to_my_model.h5')
