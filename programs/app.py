a425e880a305b0ff6f8bf14b03fcbbb7f8e1a1de
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-07 17:36:07 +0900

    img_hightとimg_widthを追加

--- programs/app.py
+++ programs/app.py
@@ -5,5 +5,12 @@ import tensorflow as tf
 import pathlib
 
 data_dir=pathlib.Path('Input Picture Path')
+img_hight=180
+img_width=180
 model = tf.models.load_model('raccoon_redpanda.h5')
 
+predict_ds=tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    subset="validation"
+    
+)
