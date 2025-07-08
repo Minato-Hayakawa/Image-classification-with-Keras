7c5912da5dae6a4220c6e63ced6ae3a3982d37af
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-08 18:16:19 +0900

    model.predict_classesを追加

--- programs/app.py
+++ programs/app.py
@@ -14,7 +14,7 @@ predict_ds=tf.keras.utils.image_dataset_from_directory(
     image_size=(img_hight,img_width),
     batch_size=4
 )
-
 class_names = predict_ds.class_names
 
 model = tf.models.load_model('raccoon_redpanda.h5')
+model.predict_classes(predict_ds)
