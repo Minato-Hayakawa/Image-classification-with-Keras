022138534049b29575c9273f1e864e6f5d579b89
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-16 18:08:51 +0900

    最終的な予測結果を表示する処理を追加しました。

--- programs/app.py
+++ programs/app.py
@@ -6,6 +6,7 @@ data_dir=pathlib.Path('Input Picture Path')
 img_hight=180
 img_width=180
 
+class_names=["raccoon","red panda"]
 predict_ds=tf.keras.utils.image_dataset_from_directory(
     data_dir,
     subset="validation",
@@ -16,7 +17,7 @@ class_names = predict_ds.class_names
 
 model = tf.models.load_model('raccoon_redpanda.h5')
 prediction=model.predict(predict_ds)
-
-print("image classification with Keras")
-
-print(prediction.shape)
+score=tf.keras.layers.softmax(prediction)*100
+ptrdicted_class_names=class_names[np.argmax(prediction[0])]
+print("This image was considered"+ptrdicted_class_names)
+print("reliability="+score)
