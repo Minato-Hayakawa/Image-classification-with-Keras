7086e63a60f26321980b7f36c03bf044210c3096
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-16 18:19:58 +0900

    predict_dsを修正

--- programs/app.py
+++ programs/app.py
@@ -9,15 +9,14 @@ img_width=180
 class_names=["raccoon","red panda"]
 predict_ds=tf.keras.utils.image_dataset_from_directory(
     data_dir,
-    subset="validation",
     image_size=(img_hight,img_width),
-    batch_size=4
 )
 class_names = predict_ds.class_names
 
 model = tf.models.load_model('raccoon_redpanda.h5')
 prediction=model.predict(predict_ds)
-score=tf.keras.nn.softmax(prediction[0])*100
+score=tf.keras.nn.softmax(prediction)*100
+reliability=np.max(score)
 predicted_class_names=class_names[np.argmax(prediction[0])]
 print("This image was considered"+predicted_class_names)
-print("reliability="+score)
+print("reliability="+reliability)
