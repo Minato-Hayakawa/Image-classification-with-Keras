65eed4355b20069f2e0aca3b702b736da1f7d628
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-17 20:59:45 +0900

    不要な処理を削除

--- programs/app.py
+++ programs/app.py
@@ -5,18 +5,14 @@ import pathlib
 data_dir=pathlib.Path('Input Picture Path')
 img_hight=180
 img_width=180
-
-class_names=["raccoon","red panda"]
 predict_ds=tf.keras.utils.image_dataset_from_directory(
     data_dir,
+    batch_size=4,
     image_size=(img_hight,img_width),
 )
 class_names = predict_ds.class_names
 
 model = tf.models.load_model('raccoon_redpanda.h5')
-prediction=model.predict(predict_ds)
-score=tf.keras.nn.softmax(prediction)*100
-reliability=np.max(score)
+prediction=model.predict(predict_ds)  
 predicted_class_names=class_names[np.argmax(prediction[0])]
 print("This image was considered"+predicted_class_names)
-print("reliability="+reliability)
