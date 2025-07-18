c4fb0cad4ea80790db9099d274497f0b830e6615
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-18 21:40:19 +0900

    画像識別の結果をプリントできるようにしました

--- programs/app.py
+++ programs/app.py
@@ -10,9 +10,14 @@ predict_ds=tf.keras.utils.image_dataset_from_directory(
     batch_size=4,
     image_size=(img_hight,img_width),
 )
-class_names = predict_ds.class_names
+class_names = ["raccoon","red panda"]
 
 model = tf.models.load_model('raccoon_redpanda.h5')
 prediction=model.predict(predict_ds)  
-predicted_class_names=class_names[np.argmax(prediction[0])]
-print("This image was considered"+predicted_class_names)
+raccoon_reliability=prediction[0].max
+redpanda_reliability=prediction[1].max
+for i in range(prediction[0].shape):
+    if prediction[i][0]>prediction[i][1]:
+        print("This image was predicted as raccoon reliability="+prediction[i][0])
+    elif prediction[i][0]<prediction[i][1]:
+        print("This image was predicted as red panda reliability="+prediction[i][0])
