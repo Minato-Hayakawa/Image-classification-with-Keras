0c8ef0cad40febf675f0660cf6dc10fcda8baeec
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-18 21:26:03 +0900

    model.summaryを追加

--- programs/train.py
+++ programs/train.py
@@ -38,6 +38,8 @@ model = tf.keras.Sequential([
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dense(num_classes)
 ])
+
+model.summary()
 model.compile(
   optimizer='adam',
   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
