b333a82c8292b53b9a2989bc2052dc1179ba5339
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-20 15:45:51 +0900

    data_dirを修正

--- programs/app.py
+++ programs/app.py
@@ -2,12 +2,12 @@ import numpy as np
 import tensorflow as tf
 import pathlib
 
-data_dir=pathlib.Path('Input Picture Path')
+data_dir=pathlib.Path('C:/vscode/python/Keras2/Image classification with Keras/newimages')
 img_hight=180
 img_width=180
 predict_ds=tf.keras.utils.image_dataset_from_directory(
     data_dir,
-    labels=None,
+    labels='inferred',
     batch_size=4,
     image_size=(img_hight,img_width),
     shuffle=False
